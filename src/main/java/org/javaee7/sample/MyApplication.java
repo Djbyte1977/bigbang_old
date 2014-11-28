9571a46503d25d9805a645aac15b786622f00186
jlzamora <jl_msg@hotmail.com>
2014-11-28 22:22:09 +0100

    singleton

--- src/main/java/org/javaee7/sample/MyApplication.java
+++ src/main/java/org/javaee7/sample/MyApplication.java
@@ -2,8 +2,16 @@ package org.javaee7.sample;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.Set;
+import java.util.HashSet;
+import org.javaee7.sample.PersonDatabase;
 
 @ApplicationPath("/resources")
 public class MyApplication extends Application {
 
+private Set<Class<?>> classes = new HashSet<Class<?>>();
+
+public MyApplication(){
+        classes.add(PersonDatabase.class); 
+   }
 }
